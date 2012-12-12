f30156580b6dff440303040604a6b36a5cc744fc
ywjno <ywjno.dev@gmail.com>
2012-12-12 10:28:53 +0800

    调整disqus和google analytics的代码位置

--- WebContent/assets/js/application.js
+++ WebContent/assets/js/application.js
@@ -16,6 +16,10 @@ $(function () {
   container.append(ul).append($('<a href="http://www.miibeian.gov.cn" target="_blank" title="查看备案信息">苏ICP备 10226088号-17</a>')).appendTo($('.footer'));
 
   qrcodeToggle("site", "显示本站QR Code", "隐藏本站QR Code");
+
+  disqus();
+
+  googleAnalytics();
 });
 
 function qrcodeToggle(id, showStr, hideStr) {
@@ -29,4 +33,29 @@ function qrcodeToggle(id, showStr, hideStr) {
       $("#" + id + "-qrcode-str").html(showStr);
     }
   );
+}
+
+function disqus() {
+  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+  var disqus_shortname = 'shortit2';
+
+  /* * * DON'T EDIT BELOW THIS LINE * * */
+  (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+}
+
+function googleAnalytics() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36753226-1']);
+  _gaq.push(['_setDomainName', 'nutz.cn']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 }
